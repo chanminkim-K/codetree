4e7400233b6a4dad93ee4546927feb32f6212094
ChanMin <103116205+chanminkim-K@users.noreply.github.com>
2026-05-14 14:10:26 +0900

    [Passed] 증가시키며 출력하기 2 (117ms, 9MB)

---
+++ trail1/증가시키며 출력하기 2/increase-and-print-2.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int start = 10;
+        int end = 26;
+
+        while(start <= end){
+            System.out.printf(start + " ");
+            start++;
+        }
+    }
+}
