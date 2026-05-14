70b5e5e6201b7f82e2e8d6863eea2ab4ceb80cf1
ChanMin <103116205+chanminkim-K@users.noreply.github.com>
2026-05-15 08:31:51 +0900

    [Passed] A/B 출력 (125ms, 10MB)

--- trail1/A/B 출력/a-divide-b.java
+++ trail1/A/B 출력/a-divide-b.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.math.BigDecimal;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,10 +6,15 @@ public class Main {
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        BigDecimal bigA = BigDecimal.valueOf(a);
-        BigDecimal bigB = BigDecimal.valueOf(b);
-        BigDecimal div = bigA.divide(bigB, 20, BigDecimal.ROUND_DOWN);
-        System.out.println(div);
+        System.out.printf(a / b + ".");
+
+        a %= b;
+        for(int i = 0; i < 20; i++){
+            a *= 10;
+            System.out.print(a / b);
+
+            a %= b;
+        }
        
     }
 }
