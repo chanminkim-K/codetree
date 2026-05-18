18405a77842ef0b614fccaf99bcf0ceabeaf08c9
ChanMin <103116205+chanminkim-K@users.noreply.github.com>
2026-05-18 23:26:49 +0900

    [Passed] 완전수 판별기 (127ms, 10MB)

--- trail1/완전수 판별기/perfect-number-discriminator.java
+++ trail1/완전수 판별기/perfect-number-discriminator.java
@@ -4,11 +4,21 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        
+        if(n == 1){
+            System.out.println("N");
+            return;
+        }
+
         int sum = 1;
 
-        for(int i = 2; i <= n/2; i++){
-            if(n % i == 0)
+        for(int i = 2; i * i <= n; i++){
+            if(n % i == 0){
                 sum += i;
+                if(i != n / i)
+                    sum += n / i;
+            }
+
             if(sum >= n)
                 break;
         }
