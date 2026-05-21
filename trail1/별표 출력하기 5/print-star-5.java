32cb2ac6655ba603a1663063f5adbb2a5b031d39
ChanMin <103116205+chanminkim-K@users.noreply.github.com>
2026-05-21 23:36:57 +0900

    [Passed] 별표 출력하기 5 (146ms, 11MB)

---
+++ trail1/별표 출력하기 5/print-star-5.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < (n - i); j++){
+                for(int k = 0; k < (n - i); k++)
+                    System.out.printf("*");
+                System.out.printf(" ");
+            }
+            System.out.println();
+        }
+    }
+}
