39f8e9782528a2976ee6c6179f4fced767a18733
ChanMin <103116205+chanminkim-K@users.noreply.github.com>
2026-05-23 13:21:03 +0900

    [Passed] 모양대로 수 출력하기 2 (146ms, 10MB)

---
+++ trail1/모양대로 수 출력하기 2/print-out-numbers-in-specific-shape-2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int cnt = 2;
+
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                if(cnt > 8)
+                    cnt= 2;
+                System.out.printf(cnt + " ");
+                cnt+=2;
+            }
+            System.out.println();
+        }
+    }
+
+}
