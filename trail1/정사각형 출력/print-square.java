5a629c9abb585d5a88a54c1b96cad47d76c69a66
ChanMin <103116205+chanminkim-K@users.noreply.github.com>
2026-05-23 13:03:08 +0900

    [Passed] 정사각형 출력 (160ms, 10MB)

---
+++ trail1/정사각형 출력/print-square.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int cnt = 1;
+
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+               System.out.printf(cnt + " ");
+               cnt++;
+            }
+            System.out.println();
+        }
+    }
+}
