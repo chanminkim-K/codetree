9e5fc64d89110be5bd790a01a756e1d27485f4d1
ChanMin <103116205+chanminkim-K@users.noreply.github.com>
2026-05-30 14:02:36 +0900

    [Passed] 시험 통과 여부 확인하기 2 (154ms, 12MB)

---
+++ trail1/시험 통과 여부 확인하기 2/verify-test-passed-2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int[] students = new int[N];
+        int cnt = 0;
+
+        for(int i = 0; i < N; i++){
+
+            double avg = 0.0;
+            for(int j = 0; j < 4; j++){
+                avg += sc.nextDouble();
+            }
+            avg /= 4;
+
+            if(avg >= 60){
+                System.out.println("pass");
+                cnt++;
+            }
+            else{
+                System.out.println("fail");
+            }
+        }
+        System.out.println(cnt);
+
+    }
+}
