8fabc8b3b44f4e0435ea8b23fca2da98eb8daf1c
ChanMin <103116205+chanminkim-K@users.noreply.github.com>
2026-07-22 22:00:08 +0900

    [Passed] 십진수로 변환하기 (122ms, 10MB)

---
+++ trail2/십진수로 변환하기/convert-to-decimal.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String binary = sc.next();
+        int answer = 0;
+        for(int i = 0; i < binary.length(); i++)
+            answer = answer * 2 + binary.charAt(i) -'0';
+    
+        System.out.println(answer);
+
+    }
+}
