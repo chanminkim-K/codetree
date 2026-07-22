6ce5864fd1a8ebd760506dd980afe88f3e449857
ChanMin <103116205+chanminkim-K@users.noreply.github.com>
2026-07-22 22:51:37 +0900

    [Passed] 진수 to 진수 (132ms, 11MB)

---
+++ trail2/진수 to 진수/transformation-of-number-system.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        String n = sc.next();
+
+        int num = 0;
+        for(int i = 0; i < n.length(); i++){
+            num = num * a + n.charAt(i) - '0'; 
+        }
+
+        while(true){
+            if(num < b){
+                sb.append(num);
+                break;
+            }
+            sb.append(num % b);
+            num /= b;
+        }
+        System.out.println(sb.reverse());
+
+    }
+}
