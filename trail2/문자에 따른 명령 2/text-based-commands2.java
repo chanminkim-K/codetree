558b9aa08b1cf9fc8214d08d813d13975605f264
ChanMin <103116205+chanminkim-K@users.noreply.github.com>
2026-07-24 13:08:32 +0900

    [Passed] 문자에 따른 명령 2 (106ms, 9MB)

---
+++ trail2/문자에 따른 명령 2/text-based-commands2.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int[] dx = new int[]{0, 1, 0, -1};
+        int[] dy = new int[]{1, 0, -1, 0};
+        int cur_dir = 0;
+        int cur_x = 0;
+        int cur_y = 0;
+
+        for(int i = 0; i < str.length(); i++){
+            char dir = str.charAt(i);
+            if(dir == 'L') 
+                cur_dir = (cur_dir - 1 + 4) % 4;
+            else if(dir == 'R') 
+                cur_dir = (cur_dir + 1) % 4;
+            else if(dir == 'F'){
+                cur_x += dx[cur_dir];
+                cur_y += dy[cur_dir];
+            }
+        }
+        System.out.println(cur_x + " " + cur_y);
+
+    }
+}
