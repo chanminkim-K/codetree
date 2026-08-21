178ecae1379fafeb6660bc0066280c12c01b6101
ChanMin <103116205+chanminkim-K@users.noreply.github.com>
2026-08-21 10:29:04 +0900

    [Passed] 연속되는 수 2 (193ms, 14MB)

---
+++ trail2/연속되는 수 2/continuous-number2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int cnt = 0;
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            if(i == 0 || arr[i - 1] == arr[i])
+                cnt++;
+            else
+                cnt = 1;
+
+            if(cnt > max)
+                max = cnt;
+            
+        }
+        System.out.println(max);
+
+    }
+}
